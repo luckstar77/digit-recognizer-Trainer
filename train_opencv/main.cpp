642050a6c53f7ce91690a15a2064c28a812a6779
luckstar77y <luckstar77y@gmail.com>
2017-06-05 23:43:46 +0800

    fix the image

--- train_opencv/main.cpp
+++ train_opencv/main.cpp
@@ -16,39 +16,39 @@ using namespace cv;
 
 int main(int argc,char** argv)
 {
-    vector<string> img_path;//输入文件名变量
+    vector<string> img_path;//輸入文件名變量
     vector<int> img_catg;
     int nLine = 0;
     string buf;
-    ifstream svm_data( "/work/shintaogas/code/train_opencv/train_opencv/num/SVM_DATA.txt" );//训练样本图片的路径都写在这个txt文件中，使用bat批处理文件可以得到这个txt文件
+    ifstream svm_data( "/work/shintaogas/code/train_opencv/train_opencv/num/gas.txt" );//訓練樣本圖片的路徑都寫在這個txt文件中，使用bat批處理文件可以得到這個txt文件
     unsigned long n;
-    while( svm_data )//将训练样本文件依次读取进来
+    while( svm_data )//將訓練樣本文件依次讀取進來
     {
         if( getline( svm_data, buf ) )
         {
             nLine ++;
-            if( nLine % 2 == 0 )//注：奇数行是图片全路径，偶数行是标签
+            if( nLine % 2 == 0 )//注：奇數行是圖片全路徑，偶數行是標籤
             {
-                img_catg.push_back( atoi( buf.c_str() ) );//atoi将字符串转换成整型，标志(0,1，2，...，9)，注意这里至少要有两个类别，否则会出错
+                img_catg.push_back( atoi( buf.c_str() ) );//atoi將字符串轉換成整型，標誌(0,1，2，...，9)，注意這裡至少要有兩個類別，否則會出錯
             }
             else
             {
-                img_path.push_back( buf );//图像路径
+                img_path.push_back( buf );//圖像路徑
             }
         }
     }
-    svm_data.close();//关闭文件
+    svm_data.close();//關閉文件
     CvMat *data_mat, *res_mat;
-    int nImgNum = nLine / 2; //nImgNum是样本数量，只有文本行数的一半，另一半是标签
-    data_mat = cvCreateMat( nImgNum, 324, CV_32FC1 );  //第二个参数，即矩阵的列是由下面的descriptors的大小决定的，可以由descriptors.size()得到，且对于不同大小的输入训练图片，这个值是不同的
+    int nImgNum = nLine / 2; //nImgNum是樣本數量，只有文本行數的一半，另一半是標籤
+    data_mat = cvCreateMat( nImgNum, 324, CV_32FC1 );  //第二個參數，即矩陣的列是由下面的descriptors的大小決定的，可以由descriptors.size()得到，且對於不同大小的輸入訓練圖片，這個值是不同的
     cvSetZero( data_mat );
-    //类型矩阵,存储每个样本的类型标志
+    //類型矩陣,存儲每個樣本的類型標誌
     res_mat = cvCreateMat( nImgNum, 1, CV_32FC1 );
     cvSetZero( res_mat );
     IplImage* src;
-    IplImage* trainImg=cvCreateImage(cvSize(28,28),8,3);//需要分析的图片，这里默认设定图片是28*28大小，所以上面定义了324，如果要更改图片大小，可以先用debug查看一下descriptors是多少，然后设定好再运行
+    IplImage* trainImg=cvCreateImage(cvSize(28,28),8,3);//需要分析的圖片，這裡默認設定圖片是28*28大小，所以上面定義了324，如果要更改圖片大小，可以先用debug查看一下descriptors是多少，然後設定好再運行
     
-    //处理HOG特征
+    //處理HOG特徵
     for( string::size_type i = 0; i != img_path.size(); i++ )
     {
         src=cvLoadImage(img_path[i].c_str(),1);
@@ -58,39 +58,39 @@ int main(int argc,char** argv)
             continue;
         }
         
-        cout<<" 处理： "<<img_path[i].c_str()<<endl;
+        cout<<" 處理： "<<img_path[i].c_str()<<endl;
         
         cvResize(src,trainImg);
         HOGDescriptor *hog=new HOGDescriptor(cvSize(28,28),cvSize(14,14),cvSize(7,7),cvSize(7,7),9);
-        vector<float>descriptors;//存放结果
-        hog->compute(trainImg, descriptors,Size(1,1), Size(0,0)); //Hog特征计算
+        vector<float>descriptors;//存放結果
+        hog->compute(trainImg, descriptors,Size(1,1), Size(0,0)); //Hog特徵計算
         cout<<"HOG dims: "<<descriptors.size()<<endl;
         n=0;
         for(vector<float>::iterator iter=descriptors.begin();iter!=descriptors.end();iter++)
         {
-            cvmSet(data_mat,i,n,*iter);//存储HOG特征
+            cvmSet(data_mat,i,n,*iter);//存儲HOG特徵
             n++;
         }
         cvmSet( res_mat, i, 0, img_catg[i] );
-        cout<<" 处理完毕: "<<img_path[i].c_str()<<" "<<img_catg[i]<<endl;
+        cout<<" 處理完畢: "<<img_path[i].c_str()<<" "<<img_catg[i]<<endl;
     }
     
     
-    CvSVM svm;//新建一个SVM
-    CvSVMParams param;//这里是SVM训练相关参数
+    CvSVM svm;//新建一個SVM
+    CvSVMParams param;//這裡是SVM訓練相關參數
     CvTermCriteria criteria;
     criteria = cvTermCriteria( CV_TERMCRIT_EPS, 1000, FLT_EPSILON );
     param = CvSVMParams( CvSVM::C_SVC, CvSVM::RBF, 10.0, 0.09, 1.0, 10.0, 0.5, 1.0, NULL, criteria );
     
-    svm.train( data_mat, res_mat, NULL, NULL, param );//训练数据
-    //保存训练好的分类器
-    svm.save( "/work/shintaogas/code/train_opencv/train_opencv/HOG_SVM_DATA.xml" );
+    svm.train( data_mat, res_mat, NULL, NULL, param );//訓練數據
+    //保存訓練好的分類器
+    svm.save( "/work/shintaogas/code/train_opencv/train_opencv/gas.xml" );
     
-    //检测样本
+    //檢測樣本
     IplImage *test;
     char result[512];
     vector<string> img_tst_path;
-    ifstream img_tst( "/work/shintaogas/code/train_opencv/train_opencv/train_test.txt" );  //加载需要预测的图片集合，这个文本里存放的是图片全路径，不要标签
+    ifstream img_tst( "/work/shintaogas/code/train_opencv/train_opencv/gas_test.txt" );  //加載需要預測的圖片集合，這個文本里存放的是圖片全路徑，不要標籤
     while( img_tst )
     {
         if( getline( img_tst, buf ) )
@@ -100,8 +100,8 @@ int main(int argc,char** argv)
     }
     img_tst.close();
     
-    ofstream predict_txt( "/work/shintaogas/code/train_opencv/train_opencv/SVM_PREDICT.txt" );//把预测结果存储在这个文本中
-    for( string::size_type j = 0; j != img_tst_path.size(); j++ )//依次遍历所有的待检测图片
+    ofstream predict_txt( "/work/shintaogas/code/train_opencv/train_opencv/gas_predict.txt" );//把預測結果存儲在這個文本中
+    for( string::size_type j = 0; j != img_tst_path.size(); j++ )//依次遍歷所有的待檢測圖片
     {
         test = cvLoadImage( img_tst_path[j].c_str(), 1);
         if( test == NULL )
@@ -113,7 +113,7 @@ int main(int argc,char** argv)
         cvZero(trainTempImg);
         cvResize(test,trainTempImg);
         HOGDescriptor *hog=new HOGDescriptor(cvSize(28,28),cvSize(14,14),cvSize(7,7),cvSize(7,7),9);
-        vector<float>descriptors;//结果数组
+        vector<float>descriptors;//結果數組
         hog->compute(trainTempImg, descriptors,Size(1,1), Size(0,0));
         cout<<"HOG dims: "<<descriptors.size()<<endl;
         CvMat* SVMtrainMat=cvCreateMat(1,descriptors.size(),CV_32FC1);
@@ -124,9 +124,9 @@ int main(int argc,char** argv)
             n++;
         }
         
-        int ret = svm.predict(SVMtrainMat);//检测结果
+        int ret = svm.predict(SVMtrainMat);//檢測結果
         sprintf( result, "%s  %d\r\n",img_tst_path[j].c_str(),ret );
-        predict_txt<<result;  //输出检测结果到文本
+        predict_txt<<result;  //輸出檢測結果到文本
     }
     predict_txt.close();
     cvReleaseMat( &data_mat );
